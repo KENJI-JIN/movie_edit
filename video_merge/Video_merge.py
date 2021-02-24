0aed4c96239eaed8b3061eebc1e8ff3b06bc991b
KENJI-JIN <60377218+KENJI-JIN@users.noreply.github.com>
2021-02-24 13:06:10 +0900

    Add files via upload

---
+++ video_merge/Video_merge.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from moviepy.editor import *
+import glob
+import argparse
+
+parser = argparse.ArgumentParser(description='merge video files') 
+parser.add_argument('--foldername', default='input_video/')
+parser.add_argument('--output_filename', default='output_video/video_merged.mp4')
+args = parser.parse_args() 
+
+folder_path=args.foldername
+save_path = args.output_filename#保存先のpath
+file_paths=glob.glob(folder_path+"*")
+
+for i, file_path in enumerate(file_paths):
+    #動画読み込み
+    tmp = VideoFileClip(file_path)
+    if i==0:
+        final_clip = tmp
+    else:
+        final_clip = concatenate_videoclips([final_clip, tmp])#動画の連結
+
+final_clip.write_videofile(save_path) #動画の書き込み
